5a943638e590336f6d06bb779f1662fbfee716d8
davidmueller13 <david.mueller1013@gmail.com>
2016-11-20 13:51:37 +0100

    add aosp vendor files

---
+++ common.mk
@@ -0,0 +1,21 @@
+
+# Bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/media/bootanimation.zip:system/media/bootanimation.zip
+
+# init.d support
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/bin/sysinit:system/bin/sysinit \
+    vendor/aosp/prebuilt/etc/init.d/00banner:system/etc/init.d/00banner \
+    vendor/aosp/prebuilt/etc/init.d/90userinit:system/etc/init.d/90userinit
+    
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+    
+# Init file
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/etc/init.local.rc:root/init.aosp.rc
+    
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/etc/spn-conf.xml:system/etc/spn-conf.xml
